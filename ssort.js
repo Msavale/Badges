7d27c85fb954d0a8a9e07d69fe24c96abcccc030
Mayur Savale <108924400+Msavale@users.noreply.github.com>
2024-03-11 05:30:26 +0530

    Create ssort.js

---
+++ ssort.js
@@ -0,0 +1,6 @@
+let numbers = [4, 2, 5, 1, 3];
+numbers.sort((a, b) => a - b); // Ascending order
+console.log(numbers); // Output: [1, 2, 3, 4, 5]
+
+// To sort in descending order:
+// numbers.sort((a, b) => b - a);
